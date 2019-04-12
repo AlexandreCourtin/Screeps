eb08c8391c114fb4f2c13b76f7ab594c64959d54
AlexandreCourtin <acourtin@student.42.fr>
2019-04-13 00:39:32 +0200

    function spawnScreep to spawn screeps

--- main.js
+++ main.js
@@ -4,31 +4,44 @@ var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
 
-    var tower = Game.getObjectById('TOWER_ID');
-    if(tower) {
-        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax
-        });
-        if(closestDamagedStructure) {
-            tower.repair(closestDamagedStructure);
-        }
+	/*var tower = Game.getObjectById('TOWER_ID');
+	if (tower) {
+		var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+			filter: (structure) => structure.hits < structure.hitsMax
+		});
+		if (closestDamagedStructure) {
+			tower.repair(closestDamagedStructure);
+		}
 
-        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if(closestHostile) {
-            tower.attack(closestHostile);
-        }
-    }
+		var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+		if (closestHostile) {
+			tower.attack(closestHostile);
+		}
+	}*/
 
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        }
-        if(creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-        if(creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
-        }
-    }
+	for (var name in Game.creeps) {
+		var creep = Game.creeps[name];
+		if (creep.memory.role == 'harvester') {
+			roleHarvester.run(creep);
+		}
+		if (creep.memory.role == 'upgrader') {
+			roleUpgrader.run(creep);
+		}
+		if (creep.memory.role == 'builder') {
+			roleBuilder.run(creep);
+		}
+	}
+}
+
+function spawnScreep(name, role) {
+	var nameRole;
+	if (role == 0)
+		nameRole = 'harvester';
+	else if (role == 1)
+		nameRole = 'upgrader';
+	else if (role == 2)
+		nameRole = 'builder';
+
+	var creep = Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], name);
+	creep.memory.role = nameRole;
 }
