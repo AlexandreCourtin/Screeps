610d4de7b2c4006c5167f6bdc42659146b607101
acourtin <acourtin@student.42.fr>
2019-04-13 11:45:41 +0200

    foreach fix

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ module.exports.loop = function () {
 		filter: (structure) => structure.structureType == STRUCTURE_TOWER
 	});
 	if (towers.length) {
-		for each (tower in towers) {
+		foreach (tower in towers) {
 			/*var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
 				filter: (structure) => structure.hits < structure.hitsMax
 			});
